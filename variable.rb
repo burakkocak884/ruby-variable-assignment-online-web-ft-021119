a7a6e6f2042cb748700c03efa03380f3c288e4e4
BURAK KOCAK <burakkocak884@hotmail.com>
2019-02-11 18:03:58 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,7 +1,6 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
- def greeting(greet)
-  greet = "Hello World"
+ 
+  greeting = "Hello World"
    
   
- end
